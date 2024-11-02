29a56b0e8c92578509983f45b859db2245a21aa2
Evaldas G. <evaldas.g@icloud.com>
2024-11-02 18:53:49 +0200

    Add errors handling #18

--- apis/services/sales/route/sys/checkapi/route.go
+++ apis/services/sales/route/sys/checkapi/route.go
@@ -5,4 +5,5 @@ import "github.com/evaldasg/ardanlabs-ultimate-go-service-k8s/foundation/web"
 func Routes(app *web.App) {
 	app.HandleFunc("GET /liveness", liveness)
 	app.HandleFunc("GET /readiness", readiness)
+	app.HandleFunc("GET /testerror", testError)
 }
